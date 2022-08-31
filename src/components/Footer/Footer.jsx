0c5db2408a09ae6f2fc2dd714dec7b3e78290b34
Tobias Selva <tobiasselva1@gmail.com>
2022-08-31 11:46:42 -0300

    adding footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,48 +5,49 @@ function Footer() {
   return (
     <footer className="wrapper">
       <div className="footer-container">
-        <div>
+        <div className="left-footer">
           <img src={logo} alt="logo" />
-          <p>
+          <p className="text">
             En este sitio encontrarás todas las universidades y sus respectivas
             calificaciones en base a lo que los usuarios opinen.{" "}
           </p>
         </div>
-        <div className="links">
-          <div>
-            <h3>About</h3>
-            <ul>
-              <li>About us</li>
-              <li>Courses</li>
-              <li>Mentor</li>
-              <li>Contact Us</li>
-            </ul>
-          </div>
-          <div>
-            <h3>Support</h3>
-            <ul>
-              <li>Careers</li>
-              <li>Help & Support</li>
-              <li>Blog</li>
-            </ul>
-          </div>
-          <div>
-            <h3>Company</h3>
-            <ul>
-              <li>Privacy Policy</li>
-              <li>Terms of Service</li>
-              <li>Code of Conduct</li>
-            </ul>
+        <div className="line"></div>
+
+        <div className="right-footer">
+          <div className="links">
+            <div className="item">
+              <h3>About us</h3>
+              <ul>
+                <li>About us</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+            <div className="item">
+              <h3>SocialMedia</h3>
+              <ul>
+                <li>Instagram</li>
+                <li>Facebook</li>
+                <li>Twitter</li>
+              </ul>
+            </div>
+            <div className="item">
+              <h3>Company</h3>
+              <ul>
+                <li>Privacy Policy</li>
+                <li>Terms of Service</li>
+                <li>Code of Conduct</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
       <div className="footer-bottom">
-        <div className="footer-text">
+        <div className="footer-left">
           <p>Política de privacidad</p>
           <p>Términos y condiciones</p>
         </div>
-
-        <div>
+        <div className="footer-right">
           <p>@2021 Dónde estudio. Todos los derechos reservados.</p>
         </div>
       </div>
