c987cf268f2ae563b5b02cc52cc17cd30f52be1a
Tobias Selva <tobiasselva1@gmail.com>
2022-09-01 15:28:51 -0300

    adding carousel

--- src/components/CardUniversity/CardUniversity.jsx
+++ src/components/CardUniversity/CardUniversity.jsx
@@ -1,19 +1,49 @@
 import React from "react";
-import img1 from "../../assets/uni1.png";
+
+import { FaStarHalfAlt, FaStar } from "react-icons/fa";
 import "./carduniversity.scss";
-function CardUniversity() {
+function CardUniversity({
+  place,
+  name,
+  img,
+  faculties,
+  careers,
+  students,
+  starts,
+}) {
   return (
     <div className="card-uni-container">
       <div className="first-row">
-        <span className="number">10</span>
-        <span>Universidad Nacional del Noroeste</span>
-        <span>Estrellas</span>
+        <span className="number">{place}</span>
+        <span>{name}</span>
+        <div className="starts">
+          {[...Array(starts)].map((start) => {
+            return (
+              <span>
+                <FaStar />
+              </span>
+            );
+          })}
+        </div>
       </div>
-      <img src={img1}/>
+      <img src={img} />
       <div className="third-row">
-        <span>10</span>
-        <span>Universidad Nacional del Noroeste</span>
-        <span>Estrellas</span>
+        <div className="data">
+          <span>{place}</span>
+          <span>place</span>
+        </div>
+        <div className="data">
+          <span>{faculties}</span>
+          <span>Faculties</span>
+        </div>
+        <div className="data">
+          <span>{careers}</span>
+          <span>Careers</span>
+        </div>
+        <div className="data">
+          <span>{students}</span>
+          <span>Students</span>
+        </div>
       </div>
     </div>
   );
--- src/components/Home/Carousel/Carousel.jsx
+++ src/components/Home/Carousel/Carousel.jsx
@@ -2,7 +2,16 @@ import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, EffectCards } from "swiper";
-
+import uni1 from "../../../assets/uni1.png";
+import uni2 from "../../../assets/uni2.png";
+import uni3 from "../../../assets/uni3.png";
+import uni4 from "../../../assets/uni4.png";
+import uni5 from "../../../assets/uni5.png";
+import uni6 from "../../../assets/uni6.png";
+import uni7 from "../../../assets/uni7.png";
+import uni8 from "../../../assets/uni8.png";
+import uni9 from "../../../assets/uni9.png";
+import uni10 from "../../../assets/uni10.png";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-cards";
@@ -28,39 +37,115 @@ export default function Carousel() {
         className="mySwiper"
       >
         <SwiperSlide>
-          <CardUniversity />
+          <CardUniversity
+            place={1}
+            name={"Universidad de Buenos Aires"}
+            img={uni1}
+            faculties={60}
+            careers={"+"+100}
+            students={100000}
+            starts={5}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <CardUniversity />
+          <CardUniversity
+            place={2}
+            name={"Universidad Nacional de la Plata"}
+            img={uni2}
+            faculties={"+"+150}
+            careers={120}
+            students={105000}
+            starts={5}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <CardUniversity />
+          <CardUniversity
+            place={3}
+            name={"Universidad de Córdoba"}
+            img={uni3}
+            faculties={70}
+            careers={"+"+100}
+            students={190000}
+            starts={5}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <CardUniversity />
+          <CardUniversity
+            place={4}
+            name={"Universidad de Nac. de Mar del Plata"}
+            img={uni4}
+            faculties={"+"+100}
+            careers={160}
+            students={244410}
+            starts={5}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <CardUniversity />
+          <CardUniversity
+            place={5}
+            name={"Universidad Nacional de Tucumán"}
+            img={uni5}
+            faculties={5}
+            careers={80}
+            students={10}
+            starts={4}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <CardUniversity />
+          <CardUniversity
+            place={6}
+            name={"Universidad Nac. del Centro de Bs As"}
+            img={uni6}
+            faculties={20}
+            careers={60}
+            students={120000}
+            starts={4}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <CardUniversity />
+          <CardUniversity
+            place={7}
+            name={"Universidad Nacional del Sur"}
+            img={uni7}
+            faculties={50}
+            careers={95}
+            students={12450}
+            starts={4}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <CardUniversity />
+          <CardUniversity
+            place={8}
+            name={"Universidad Nacional de Quilmes"}
+            img={uni8}
+            faculties={+100}
+            careers={90}
+            students={100000}
+            starts={4}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <CardUniversity />
+          <CardUniversity
+            place={9}
+            name={"Universidad Nacional de Rosario"}
+            img={uni9}
+            faculties={"+"+100}
+            careers={60}
+            students={10233}
+            starts={3}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <CardUniversity />
+          <CardUniversity
+            place={10}
+            name={"Universidad Nacional del Nordeste"}
+            img={uni10}
+            faculties={13}
+            careers={+100}
+            students={140000}
+            starts={3}
+          />
         </SwiperSlide>
-        <SwiperSlide>
-          <CardUniversity />
-        </SwiperSlide>
-       
       </Swiper>
     </>
   );
