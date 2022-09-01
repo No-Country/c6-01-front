0f1b45abdff9f388edbc0e6e54bbba3d1558f39f
Tobias Selva <tobiasselva1@gmail.com>
2022-09-01 20:33:55 -0300

    adding router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import { useState } from "react";
-import Home from "./components/Home/Home";
-import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
-import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-//import Navbar from "./components/Navbar/Navbar";
 import Navbar from "./components/Navbar/Navbar";
-
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import RouterManager from "./components/router/RouterManager";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      
+      <BrowserRouter>
+        <Navbar />
+        <RouterManager />
+      </BrowserRouter>
       <Footer />
     </>
   );
